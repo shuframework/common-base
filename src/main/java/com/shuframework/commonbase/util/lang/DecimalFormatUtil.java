f3b5f147b6e0b285ecb7a8e2fa5c28fbc79eecb7
shuheng <shuheng@rangercn.com>
2019-02-27 14:47:29 +0800

    double类型的格式化工具类

---
+++ src/main/java/com/shuframework/commonbase/util/lang/DecimalFormatUtil.java
@@ -0,0 +1,7 @@
+package com.shuframework.commonbase.util.lang;
+
+/**
+ * @author shuheng
+ */
+public class DecimalFormatUtil {
+}
---
+++ src/test/java/com/shuframework/commonbase/util/lang/DecimalFormatUtilTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author shuheng
+ */
+public class DecimalFormatUtilTest {
+
+}
